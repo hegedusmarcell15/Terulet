20eb255091d2f8dea606b3d78de8c2ece17f0d84
hegedusmarcell15 <hegedusm@kkszki.hu>
2024-09-20 13:08:56 +0200

    The result is is complete

--- Terulet/Program.cs
+++ Terulet/Program.cs
@@ -16,9 +16,25 @@ namespace Terulet
             int b;
             int t;
 
-            a =  Convert.ToInt32(Console.ReadLine());
-            b = Convert.ToInt32(Console.ReadLine());
-            t = a*b;
+
+            do
+            {
+                Console.Write("Kérem az A oldal értékét (0> kell lennie): ");
+                a = Convert.ToInt32(Console.ReadLine());
+            } while (a<=0);
+
+
+            do
+            {
+                Console.Write("Kérem az B oldal értékét (0> kell lennie): ");
+                b = Convert.ToInt32(Console.ReadLine());
+            } while (b<=0);
+
+
+
+            t = a * b;
+
+            Console.WriteLine($"A téglalap területe: {t}");
         }
         static void Main(string[] args)
         {
